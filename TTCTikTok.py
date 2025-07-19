4055b95a4886d66e239fe8e4b4a4aa2b482e685c
tsanduongvi123 <duongvitsan123@gmail.com>
2025-07-19 19:41:24 +0700

    Update TTCTikTok.py

--- TTCTikTok.py
+++ TTCTikTok.py
@@ -1,8 +1,9 @@
 import os
 
 def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
+    os.system('cls' if os.name == 'nt'
+else 'clear')
+	
 def banner():
 	print(f"""
 \033[1;31m██████╗░██╗░░░██╗██╗  ████████╗░█████╗░░█████╗░██╗░░░░░
